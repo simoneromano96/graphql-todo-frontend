ed8446d9fbdf902bf5c72644d040e292152da849
Simono il magnifico <simoneromano@tutanota.de>
2020-12-05 13:53:05 +0100

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,12 @@
 <template>
   <div class="app dark flex flex-col bg-gradient-to-br from-blue-300 to-indigo-600">
-    <h1 class="text-center m-4 text-xl font-bold">Todos</h1>
-    <div v-if="error">{{ error.message }}</div>
-    <Suspense v-else>
-      <template #default>
-        <todo-list />
-      </template>
-      <template #fallback>
-        <div>Loading...</div>
-      </template>
-    </Suspense>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onErrorCaptured, onMounted, ref } from "vue"
 
-import TodoList from "./components/TodoList.vue"
-
-const App = defineComponent({
-  components: {
-    TodoList,
-  },
-  setup(props, context) {
-    const error = ref(null)
-
-    onErrorCaptured((caughtError) => {
-      error.value = caughtError
-      return true
-    })
-
-    return { error }
-  },
-})
+const App = defineComponent({})
 
 export default App
 </script>
